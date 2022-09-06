07a30129f2da1efdcef7c69f3b426945b24a6992
“preetimca2016” <preetimca2016@gmail.com>
2022-09-06 08:00:25 +0530

    Add: insertionSort Algorithm

---
+++ Sorting_SearchingAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sorting_SearchingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSortAlgo.PrintArray();
+            Console.ReadKey();
+            
+        }
+    }
+}
