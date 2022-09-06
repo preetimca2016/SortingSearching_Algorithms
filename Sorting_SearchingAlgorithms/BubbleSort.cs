6d8a3a2eeaafd007c9780815f75805e718b096dc
“preetimca2016” <preetimca2016@gmail.com>
2022-09-06 08:23:35 +0530

    Add:Bubble Sort Algorithm

---
+++ Sorting_SearchingAlgorithms/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_SearchingAlgorithms
+{
+    class BubbleSort
+    {
+        public static void PrintArray()
+        {
+            int[] Arrays = { 34, 67, 45, 12, 89 };
+            BubbleSortAlgo(Arrays);
+            foreach (int List in Arrays)
+            {
+                Console.WriteLine(List);
+            }
+        }
+        public static void BubbleSortAlgo(int[] Arrays)
+        {
+            int n = Arrays.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (Arrays[j] > Arrays[j + 1])
+                    {
+                        // swap temp and Arrays[i]
+                        int temp = Arrays[j];
+                        Arrays[j] = Arrays[j + 1];
+                        Arrays[j + 1] = temp;
+                    }
+        }
+    }
+}
--- Sorting_SearchingAlgorithms/Program.cs
+++ Sorting_SearchingAlgorithms/Program.cs
@@ -6,8 +6,21 @@ namespace Sorting_SearchingAlgorithms
     {
         static void Main(string[] args)
         {
-            InsertionSortAlgo.PrintArray();
-            Console.ReadKey();
+            Console.WriteLine("Enter 1 For Insertion Sort");
+            Console.WriteLine("Enter 2 For Bubble Sort");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    InsertionSortAlgo.PrintArray();
+                    Console.ReadKey();
+                    break;
+                    case 2:
+                    BubbleSort.PrintArray();
+                    Console.ReadKey();
+                    break;
+            }
+            
             
         }
     }
